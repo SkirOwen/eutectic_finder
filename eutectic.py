4d6c8d12c0a265b430388415743cce04e861f097
Owen <owen.allemang@gmail.com>
2023-07-17 11:11:12 +0100

    fixed the solving and added rich prints

--- eutectic.py
+++ eutectic.py
@@ -1,11 +1,14 @@
 import numpy as np
 from scipy.optimize import fsolve
-from time import time
+from rich import print
+from rich.panel import Panel
+from rich.console import Console
+from rich.table import Table
 
 from utils import get_lines_for_run
 
 
-def equations(variables) -> list:
+def equations(variables, Hi, Ti) -> list:
 	# xi, T = variables
 	xi = variables[:-1]
 	T = variables[-1]
@@ -22,56 +25,67 @@ def equations(variables) -> list:
 	return equations
 
 
-def find_eutectic() -> np.ndarray:
-	start = time()
+def find_eutectic(Hi, Ti) -> tuple:
 	initial_guess = np.ones(len(Hi) + 1) / (len(Hi) + 1)
 	initial_guess[-1] = Ti[0]
 
 	# Solve the equations
-	solution = fsolve(equations, x0=initial_guess)
-	stop = time()
-	print(stop - start)
-
-	xi = solution[:-1]
-	T = solution[-1]
-
-	print("xi:", xi)
-	print("T:", T - 273.15)
-
+	solution = fsolve(equations, x0=initial_guess, args=(Hi, Ti))
 	return solution
 
 
-def main():
-	# Hi = np.array([11700, 25500, 15900])  # Example values
-	run = 1
-	use_celsius = True
+def run_eutectic(Hi, Ti, use_celsius) -> None:
+	"""Wrapper for printing information"""
 
-	global Hi
-	global Ti
+	temp_unit = "C" if use_celsius else "K"
+	print(Panel.fit(
+		f"Enthalpies (J/mol):  {', '.join(map(str, Hi))}\n"
+		f"Temperatures ({temp_unit}):    {', '.join(map(str, Ti))}",
+		title="[green]Eutectic Finder",
+	))
+	print(f"Using Kelvin: {not use_celsius}\n")
 
-	run_info, mixture = get_lines_for_run("data.csv", run_number=run)
-	mixture = np.array(mixture)
+	solution = find_eutectic(Hi, Ti)
+	xi = solution[:-1]
+	T = solution[-1]
+
+	table = Table(title="Eutectic")
+	table.add_column("Variable", justify="left", no_wrap=True, style="cyan")
+	table.add_column("Value", justify="right", no_wrap=True, style="red")
+	for i, x in enumerate(xi):
+		table.add_row(
+			f"x_{i}",
+			f"{x:.5f}"
+		)
+	table.add_row(
+		f"Temp Eutectic (C)",
+		f"{T - 273.15:.5f}"
+	)
+	console = Console()
+	console.print(table)
+
+
+def run_from_csv(filename: str, run: int, use_celsius: bool):
+	run_info, mixture = get_lines_for_run(filename, run_number=run)
+	print(f"Using Run #{run}")
+
+	print(f"Elements: [green]{', '.join(map(str, run_info[:, 1]))}[/green]")
 
 	Hi = mixture[:, 0]
 	if use_celsius:
-		Ti = mixture[:, 1] + 273.15
+		Ti = mixture[:, 1]
+		if (Ti == -1).any():
+			raise ValueError("Celsius temperature not defined")
+		Ti += 273.15
 	else:
 		Ti = mixture[:, 2]
+		if (Ti == -1).any():
+			raise ValueError("Kelvin temperature not defined")
+	run_eutectic(Hi, Ti, use_celsius)
 
-	print(Hi)
-	print(Ti)
-	# # Set the values for Hi and Ti
-	# Hi = np.array([11300, 46000])  # Example values
-	# Ti = np.array([1234, 1688])  # Example values
-	#
-	# Hi = np.array([25500, 13400, 28500])  # Example values
-	# Ti = np.array([772, 610, 808]) + 273.15  # Example values
 
-	# Ti = np.array([337, 254, 310]) + 273.15  # Example values
-
-	# Initial guess for xi and T
-	# initial_guess = np.array([[0.5, 0.5], [1234, 1688]])
-	find_eutectic()
+def main():
+	run_from_csv("data.csv", 1, False)
 
 
 if __name__ == "__main__":
